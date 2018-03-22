f70f2aa918baf725f7a2ec3aaba269603353f100
Paige Brown <brown.emily.paige@gmail.com>
2018-03-22 14:00:30 -0400

    30 minute break

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,7 +1,10 @@
 (function() {
     function HomeCtrl($firebaseArray, $interval) {
-      this.clock = 3;
+      this.clock = 2;
       this.onBreak = false;
+
+      var completedSessions = 0;
+
       this.timer = null;
       this.buttonMsg = "Start Time";
       this.breakMsg = "Start Break";
@@ -12,11 +15,20 @@
         this.buttonMsg = "Reset Time";
         this.timer = $interval(function(){
           this.clock -= 1;
-          if(this.clock == 0){
+          if(this.clock == 0 && completedSessions == 3){
+            $interval.cancel(this.timer);
+            this.timerRunning = false;
+            this.breakMsg = "Start 30 Minute Break"
+            this.onBreak = true;
+            this.clock = 30;
+            completedSessions = 0;
+          }
+          else if(this.clock == 0) {
             $interval.cancel(this.timer);
             this.timerRunning = false;
             this.onBreak = true;
-            this.clock = 300;
+            this.clock = 5;
+            completedSessions ++;
           }
         }.bind(this), 1000);
       }
@@ -24,7 +36,7 @@
       this.resetTimer = function(){
         if(angular.isDefined(this.timer)){
           $interval.cancel(this.timer);
-          this.clock = 1500;
+          this.clock = 2;
           this.timerRunning = false;
           this.buttonMsg = "Start Time";
         }
@@ -34,13 +46,14 @@
         this.timerRunning= true;
         this.onBreak = false;
         this.buttonMsg = "Reset Time";
+        this.breakMsg = "Start Break";
         this.timer = $interval(function (){
           this.clock -=1;
           if(this.clock == 0) {
             $interval.cancel(this.timer);
             this.timerRunning = false;
             this.buttonMsg = "Start Time";
-            this.clock = 1500;
+            this.clock = 2;
           }
         }.bind(this), 1000);
       }
